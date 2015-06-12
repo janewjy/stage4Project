bc5ad49d581b624b9dfb0ef1217ecc54d0fe7c70
jingya <janewjy87@gmail.com>
2015-06-12 16:56:33 -0700

    project 4 ready

--- main.py
+++ main.py
@@ -16,41 +16,109 @@
 #
 import os
 
+import cgi
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb          # To use the data modeling API, imports the google.appengine.ext.ndb module
+
+
 import jinja2
 import webapp2
 
 template_dir = os.path.join(os.path.dirname(__file__),'template')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 
+DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
+
+# We set a parent key on the 'Greetings' to ensure that they are all
+# in the same entity group. Queries across the single entity group
+# will be consistent.  However, the write rate should be limited to
+# ~1/second.
+
+def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+    """Constructs a Datastore key for a Guestbook entity.
+
+    We use guestbook_name as the key.
+    """
+    return ndb.Key('Guestbook', guestbook_name)
 
 
+class Author(ndb.Model):
+    """Sub model for representing an author."""
+    identity = ndb.StringProperty(indexed=False)
+    email = ndb.StringProperty(indexed=False)
 
-class Handler(webapp2.RequestHandler):
-    def write(self,*a,**kw):
-        self.response.out.write(*a,**kw)
 
-    def render_str(self,template,**params):
-    	t = jinja_env.get_template(template)
-    	return t.render(params)
+class Greeting(ndb.Model):
+    """A main model for representing an individual Guestbook entry."""
+    author = ndb.StructuredProperty(Author)
+    content = ndb.StringProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
 
-    def render(self, template, **kw):
-    	self.write(self.render_str(template, **kw))
 
-class MainPage(Handler):
+class MainPage(webapp2.RequestHandler):
     def get(self):
-        items = self.request.get_all("food")
-        print items
-        self.render("shopping_list.html", items = items)
-        
-# class FizzBuzzHandler(Handler):
-#     def get(self):
-#         n = self.request.get("n",0)
-#         if n:
-#             n = int(n)
-#         self.render("fizzbuzz.html",n=n)
-		
+        guestbook_name = self.request.get('guestbook_name',
+                                          DEFAULT_GUESTBOOK_NAME)
 
-app = webapp2.WSGIApplication([('/', MainPage),
-    # ('/fizzbuzz',FizzBuzzHandler),
-    ], debug=True)
+        # Ancestor Queries, as shown here, are strongly consistent
+        # with the High Replication Datastore. Queries that span
+        # entity groups are eventually consistent. If we omitted the
+        # ancestor from this query there would be a slight chance that
+        # Greeting that had just been written would not show up in a
+        # query.
+        greetings_query = Greeting.query(
+            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+        greetings = greetings_query.fetch(10)
+
+        user = users.get_current_user()
+
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        # Write the submission form and the footer of the page
+        sign_query_params = urllib.urlencode({'guestbook_name':
+                                              guestbook_name})
+        template_values = {
+            'user': user,
+            'greetings': greetings,
+            'guestbook_name': urllib.quote_plus(guestbook_name),
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = jinja_env.get_template('stage4.html')
+        self.response.write(template.render(template_values))
+
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        # We set the same parent key on the 'Greeting' to ensure each
+        # Greeting is in the same entity group. Queries across the
+        # single entity group will be consistent. However, the write
+        # rate to a single entity group should be limited to
+        # ~1/second.
+        guestbook_name = self.request.get('guestbook_name',
+                                          DEFAULT_GUESTBOOK_NAME)
+        greeting = Greeting(parent=guestbook_key(guestbook_name))
+
+        if users.get_current_user():
+            greeting.author = Author(
+                    identity=users.get_current_user().user_id(),
+                    email=users.get_current_user().email())
+
+        greeting.content = self.request.get('content')
+        greeting.put()
+
+        query_params = {'guestbook_name': guestbook_name}
+        self.redirect('/?' + urllib.urlencode(query_params))
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/sign', Guestbook),
+], debug=True)
